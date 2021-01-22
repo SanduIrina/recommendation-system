3fc1f255d10a69641003fd16ae31f0da19106a81
Irina Sandu <sandu.irina2@gmail.com>
2021-01-22 23:56:44 +0200

    Adding collab filtering file

---
+++ collab-filtering/predict.py
@@ -0,0 +1,14 @@
+from surprise import KNNBasic
+
+# load data
+# train model
+
+def get_predictions_for_user(user_id, mode):
+    if mode='song':
+        pass
+    if mode='cluster':
+        pass
+    return
+
+def update_recommendations_matrix(user, song, rating=None):
+    pass
